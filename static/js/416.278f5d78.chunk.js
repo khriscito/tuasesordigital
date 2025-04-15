09df25430ec705c9aab21c709afaae085a9ee369
khriscito <khristhopher100@hotmail.com>
2025-04-15 05:07:51 -0400

    Updates

---
+++ static/js/416.278f5d78.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[416],{9416:(e,n,i)=>{i.r(n),i.d(n,{default:()=>h});var t=i(1319),d=i(4647),r=i(9281),s=i(3614),c=i(409),a=i(9);const l=(0,a.Ay)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,m=(0,a.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,p=(0,a.Ay)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;var x=i(579);const h=(0,r.C)()((e=>{let{title:n,content:i,button:r,t:a}=e;return(0,x.jsx)(l,{children:(0,x.jsx)(s.q7,{direction:"up",triggerOnce:!0,children:(0,x.jsx)(t.A,{justify:"center",align:"middle",children:(0,x.jsx)(p,{children:(0,x.jsxs)(d.A,{lg:24,md:24,sm:24,xs:24,children:[(0,x.jsx)("h6",{children:a(n)}),(0,x.jsx)(m,{children:a(i)}),r&&(0,x.jsx)(c.$,{name:"submit",onClick:()=>window.open("https://forms.kommo.com/rdwrtdd","_blank"),children:a(r)})]})})})})})}))}}]);
+//# sourceMappingURL=416.278f5d78.chunk.js.map
---
+++ static/js/566.197d2689.chunk.js
@@ -0,0 +1,45 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[566],{6566:(e,i,t)=>{t.r(i),t.d(i,{default:()=>w});var n=t(4647),d=t(1319),s=t(3614),c=t(9281),r=t(409),o=t(2646),a=t(9);const l=(0,a.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,p=(0,a.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,h=(0,a.Ay)(d.A)`
+  flex-direction: ${e=>{let{direction:i}=e;return"left"===i?"row":"row-reverse"}};
+`,m=(0,a.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,x=(0,a.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,f=(0,a.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,j=(0,a.Ay)("p")`
+  font-size: 13px;
+`,y=(0,a.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`;var u=t(579);const w=(0,c.C)()((e=>{let{icon:i,title:t,content:c,section:a,button:w,t:g,id:A,direction:v}=e;return(0,u.jsx)(l,{children:(0,u.jsx)(s.zW,{direction:v,triggerOnce:!0,children:(0,u.jsxs)(h,{justify:"space-between",align:"middle",id:A,direction:v,children:[(0,u.jsx)(n.A,{lg:11,md:11,sm:12,xs:24,children:(0,u.jsx)(o.A,{src:i,width:"100%",height:"100%"})}),(0,u.jsx)(n.A,{lg:11,md:11,sm:11,xs:24,children:(0,u.jsxs)(m,{children:[(0,u.jsx)("h6",{children:g(t)}),(0,u.jsx)(p,{children:g(c)}),"right"===v?(0,u.jsx)(y,{children:"object"===typeof w&&w.map(((e,i)=>(0,u.jsx)(r.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("about"),children:g(e.title)},i)))}):(0,u.jsx)(x,{children:(0,u.jsx)(d.A,{justify:"space-between",children:"object"===typeof a&&a.map(((e,t)=>(0,u.jsxs)(n.A,{span:11,children:[(0,u.jsx)(o.A,{src:e.icon,width:"60px",height:"60px"}),(0,u.jsx)(f,{children:g(e.title)}),(0,u.jsx)(j,{children:g(e.content)}),"documentos.avif"===i&&(0,u.jsx)(r.$,{onClick:()=>{const i={"Gaceta Oficial":"documents/Gaceta.pdf","Unidades de Medida":"/Mediciones.pdf",Providencia:"/Providencia.pdf"}[e.title];if(i){const e=document.createElement("a");e.href=i,e.download=i.split("/").pop(),e.click()}},children:"Descargar"})]},t)))})})]})})]})})})}))}}]);
+//# sourceMappingURL=566.197d2689.chunk.js.map
